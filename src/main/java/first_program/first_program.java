2c21d11a897f2a3458cac3b92c110bc026426369
Eko Yulian <ekoyulianto436@gmail.com>
2023-10-21 01:14:13 +0700

    Create Java Basic Example

---
+++ src/main/java/first_program/first_program.java
@@ -0,0 +1,10 @@
+package first_program;
+
+public class first_program {
+    public static void main(String[] args){
+//        ini adalah print Hello World pada class first_program
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}
